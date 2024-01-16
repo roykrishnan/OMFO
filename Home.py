56cc77ede5a83cc9d2f51428ee9c229efbb12d3f
Roy Krishnan <60521440+roykrishnan@users.noreply.github.com>
2024-01-15 20:06:25 -0500

    Add files via upload

--- Home.py
+++ Home.py
@@ -21,12 +21,12 @@ with st.expander("**Why build this?**"):
     st.markdown(""" 
             I broke into the NBA 2K League at 22, a month after I graduated college and after I founded my company Liquid Sports Lab and our NBA 2K Team Liquid Pro Am in January 2021. 
             Liquid Pro Am was always just supposed to be an experiment of what analytics could do in e-sports, but instead it became part of my way to break into the NBA. 
-            After a stint as an Assistant and the Offensive Co-Ordinator with the Dallas Mavericks and Mavs Gaming, I became the GM and Head Coach of the Toronto Raptors' - Raptors Uprising in 2023 where statistical thinking
+            After a stint as an Assistant Coach and the Offensive Co-Ordinator with the Dallas Mavericks and Mavs Gaming, I became the GM and Head Coach of the Toronto Raptors' - Raptors Uprising in 2023 where statistical thinking
             became my leverage point in systematically engineering an honest rebuild plan to be a playoff contender in two years. 
             
-            The global problem? Objectivity is not always invested in, simply because the internal ecosystem is too small and therefore has to run like a startup, where every dollar counts.
+            The global problem? Objectivity is not always invested in, simply because the 2K League internal ecosystem is too small and therefore has to run like a startup, where every dollar counts.
             Even if you invested in engineering, the $200,000 an organization would have to spend on talented help and a multi-faceted front office wouldn't be able to be marginally regenerated (in terms of profit) even if the players won every tournament.
-            As a result, all front office operations are a one or two man job; all good coaches end up working 10-12 hour days in season, usually 6-7 days a week and information is often forced to the backseat just because so much more needs to be done.
+            As a result, all front office operations are a one or two man job; all good coaches end up working 10-12 hour days and usually 6-7 days a week and therefore information is often forced to the backseat just because so much more needs to be done.
             
             Therefore, I created the One Man Front Office. A reflection of my time and thinking in the NBA 2K League in an effort to bring more information to the 2KL staff, player, fan or analyst's pocket. 
             This game will always be played by players, and the analytics are not here to prove a point, they are here to keep each and all honest about who they are today and how they can
@@ -44,7 +44,7 @@ with st.expander("**Premium Model**"):
             amateur 2K and professional 2K, we'll be able to answer questions like 'how do you know amateur talent is going to translate to the league?'
             
             We'll also have: 
-            - Rotation Rercentages (How often does each team Stack/Triangle/Circle etc.?)
+            - Rotation Percentages (How often does each team Stack/Triangle/Circle etc.?)
             - Off-Ball Defender status (Who do you want switched on ball?)
             - PG Habits: What are the opposing PG's tendencies?
                 - For example: in the Turn Tournament last year one PG has shot 20 fades in 5v5: 17 fades from the left wing, 3 from the right wing. He 10/17 on the left fade, 0/3 going right. My team forced him to fade to the right (his off hand) every time we played him (knowing he wouldn't).
@@ -66,7 +66,7 @@ with st. expander("**You want to be in the 2K League? Here's five things you nee
              
             3. Know exactly where you bring value. In one sentence be able to convey what you bring to the table that someone else either cannot, or cannot at your level.
              
-            4. The only reason you should be doing this job is because you love it. If you are using the 2K League as a resume builder I promise you 80% of the time it will not work (unless that other thing you want to do is also in Esports).
+            4. The only reason you should be doing this job is because you love it. If you are using the 2K League as a resume builder I promise you it will not usually work (unless that other thing you want to do is also in Esports).
             
             5.  Keep perspective. **The most important thing I can tell you**: every time you have a bad day, take deep breath, collect your emotions and remind yourself of why you do this in the first place, the fact that a younger you would not believe
              the fact you get to do this for a living. The second it stops feeling like a passion and starts feels like a chore is the first sign that its time to graciously and gracefully move on to what
